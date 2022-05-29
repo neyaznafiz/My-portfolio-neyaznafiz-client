339ab23c76995261baf1c183bbc79ce28f45bdab
niiyazobug <niiyaz5555@gmail.com>
2022-05-29 14:19:01 +0600

    material setup

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#ffffff",
+          "secondary": "#dca54c",
+          "accent": "#d69224",
+          "neutral": "#3D4451",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require('daisyui')],
+} 
