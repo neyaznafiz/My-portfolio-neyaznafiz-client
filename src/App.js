2e87974a4f9c837b6e22a8bbb8ac18d9394eebc5
niiyazobug <niiyaz5555@gmail.com>
2022-06-04 16:27:37 +0600

    loading hook created and use it for opening the website

--- src/App.js
+++ src/App.js
@@ -8,25 +8,38 @@ import Home from "./Pages/Home";
 import Blog from "./Pages/Blog";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from "react-toastify";
+import { PuffLoader } from "react-spinners";
+import useLoading from "./Hooks/useLoading";
 
 
 function App() {
+
+  const [loading]=useLoading()
+
   return (
-    <div className="bg-neutral">
+    <div className="bg-neutral h-screen">
 
-        
-      <Navbar>
-        <Routes>
+      {
+        loading ?
+          <div className="grid items-center justify-center h-screen">
+            <PuffLoader color='#ffffff' loading={loading} size={150} width='300' />
+          </div>
+          :
+          <>
+            <Navbar>
+              <Routes>
 
-          <Route path="/" element={<Home></Home>} />
-          {/* <Route path='/about' element={<About></About>} /> */}
-          <Route path='/portfolio' element={<Portfolio></Portfolio>} />
-          <Route path='/blog' element={<Blog></Blog>} />
-          <Route path='/contact' element={<Contact></Contact>} />
+                <Route path="/" element={<Home></Home>} />
+                {/* <Route path='/about' element={<About></About>} /> */}
+                <Route path='/portfolio' element={<Portfolio></Portfolio>} />
+                <Route path='/blog' element={<Blog></Blog>} />
+                <Route path='/contact' element={<Contact></Contact>} />
 
-        </Routes>
-      </Navbar>
-      <ToastContainer/>
+              </Routes>
+            </Navbar>
+            <ToastContainer />
+          </>
+      }
     </div>
   );
 }
