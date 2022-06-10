c9be05c35349cb3462f76ef750c20c519191b3ec
niiyazobug <niiyaz5555@gmail.com>
2022-06-10 21:57:31 +0600

    product short info added

--- src/Pages/Portfolio/Projects/Projects.js
+++ src/Pages/Portfolio/Projects/Projects.js
@@ -18,14 +18,15 @@ const Projects = () => {
 
 
     return (
-        <div>
-
+        <div className='flex justify-center'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-10 md:w-[1600px] py-20 px-6 md:px-10'>
             {
                 myProjects.map(project => <DisplayAllProjects
                     key={project._id}
                     project={project}
                 />)
             }
+        </div >
         </div>
     );
 };
